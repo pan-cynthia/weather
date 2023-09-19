4e620f3b3670c985366c3f587ddf9600bded31c1
Cynthia Pan <cynthiapan11@gmail.com>
2023-09-18 19:42:59 -0700

    add search bar + temperature unit buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Bookmarks from './components/Bookmarks.jsx'
+import Search from './components/Search.jsx'
 
 function App() {
   return (
     <div className='mx-auto max-w-screen-md mt-5 px-32 py-5 rounded-lg bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400'>
       <Bookmarks/>
+      <Search/>
     </div>
   )
 }
---
+++ src/components/Search.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { UilSearch } from '@iconscout/react-unicons'
+
+function Search() {
+  return (
+    <div className='mx-auto flex justify-around my-6 w-5/6'>
+      <div className='flex w-5/6 space-x-2 bg-white rounded-sm'>
+        <UilSearch className='text-slate-400 m-2' size={25}/>
+        <input type="text" placeholder='Search' className='outline-none'/>
+      </div>
+      <div className='flex justify-center items-center w-1/6 space-x-2 text-white'>
+        <button name='metric'>°C</button>
+        <p>|</p>
+        <button name='imperial'>°F</button>
+      </div>
+    </div>
+  )
+}
+
+export default Search
