a2724cfd754be760ff75856829ae9bc6d1aab883
Cynthia Pan <cynthiapan11@gmail.com>
2023-09-19 14:55:23 -0700

    remove city bookmarks

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import Bookmarks from './components/Bookmarks.jsx'
 import Search from './components/Search.jsx'
 import DateTime from './components/DateTime.jsx'
 import Details from './components/Details.jsx'
@@ -7,7 +6,6 @@ import Forecast from './components/Forecast.jsx'
 function App() {
   return (
     <div className='mx-auto max-w-screen-md mt-5 px-32 py-5 rounded-lg bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400'>
-      <Bookmarks/>
       <Search/>
       <DateTime/>
       <Details/>
