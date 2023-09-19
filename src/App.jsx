1aad5869657957495afe7f05da6610d5262b6766
Cynthia Pan <cynthiapan11@gmail.com>
2023-09-18 19:48:03 -0700

    add date and time placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Bookmarks from './components/Bookmarks.jsx'
 import Search from './components/Search.jsx'
+import DateTime from './components/DateTime.jsx'
 
 function App() {
   return (
     <div className='mx-auto max-w-screen-md mt-5 px-32 py-5 rounded-lg bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400'>
       <Bookmarks/>
       <Search/>
+      <DateTime/>
     </div>
   )
 }
---
+++ src/components/DateTime.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function DateTime() {
+  return (
+    <div className='flex justify-center items-center text-white'>Monday, Sep 18 | Local Time: 06:22pm</div>
+  )
+}
+
+export default DateTime
