45ccd97c7dfd5681049cc23d33ace19259954eba
Cynthia Pan <cynthiapan11@gmail.com>
2023-09-19 19:42:57 -0700

    change text color of forecast headers + dividers

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -2,12 +2,11 @@ import React from 'react'
 
 function Forecast({title}) {
   return (
-    <div>
-      <div className='mt-6'>
-        <p className='text-white font-medium uppercase'>{title}</p>
-        <hr className='my-2'/>
+    <div className='bg-[#0478c9] rounded-lg p-5 m-10'>
+      <div className='flex items-center mb-2 space-x-1 text-[#7eb8e0]'>
+        <p className=' font-medium uppercase'>{title}</p>
       </div>
-      <div className='grid grid-cols-5 divide-x text-white'>
+      <div className='grid grid-cols-5 divide-x divide-[#7eb8e0]/25 text-white'>
         <div className='flex flex-col items-center'>
           <p>02:00 PM</p>
           <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="sunny" className='w-12 my-1'/>
