462bf24c4d797b8e3b57d4e665726ed59ce712cf
Cynthia Pan <cynthiapan11@gmail.com>
2023-09-20 14:09:18 -0700

    reformat weather conditions display

--- src/Conditions.jsx
+++ src/Conditions.jsx
@@ -3,60 +3,67 @@ import { UilArrowUp, UilArrowDown, UilTemperature, UilTear, UilWind, UilSun, Uil
 
 function Conditions() {
   return (
-    <div className='bg-[#0478c9] rounded-lg p-5 m-10'>
+    <div className='bg-[#0478c9] rounded-lg p-5 w-2/6'>
       <div className='flex items-center space-x-1.5 text-[#7eb8e0]'>
         <UilInfoCircle size={18}/>
         <p className='mb-2 font-medium uppercase'>Conditions</p>
       </div>
-      <div className='grid grid-cols-4 gap-6 text-white'>
-        <div>
-          <div className='flex items-center space-x-1'>
+      <div className='grid grid-cols-2 gap-5 text-white'>
+        <div className='bg-[#7eb8e0]/30 rounded-xl flex flex-col items-center justify-center py-2'>
+          <div className='flex items-center'>
             <UilArrowUp size={20}/>
             <p>High</p>
           </div>
-          <p className='text-2xl ml-6'>69°</p>
+          <p className='text-2xl'>&nbsp;69°</p>
         </div>
-        <div>
-          <div className='flex items-center space-x-1'>
+        <div className='bg-[#7eb8e0]/30 rounded-xl flex flex-col items-center justify-center py-2'>
+          <div className='flex items-center'>
             <UilArrowDown size={20}/>
             <p>Low</p>
           </div>
-          <p className='text-2xl ml-6'>58°</p>
+          <p className='text-2xl'>&nbsp;58°</p>
         </div>
-        <div>
-          <div className='flex items-center space-x-1'>
-            <UilSun size={18}/>
-            <p>Sunrise</p>
-          </div>
-          <p className='text-2xl ml-6'>06:54 AM</p>
-        </div>
-        <div>
-          <div className='flex items-center space-x-1'>
-            <UilSunset size={18}/>
-            <p>Sunset</p>
-          </div>
-          <p className='text-2xl ml-6'>07:13 PM</p>
-        </div>
-        <div>
-          <div className='flex items-center space-x-1'>
+        <div className='bg-[#7eb8e0]/30 rounded-xl flex flex-col items-center justify-center py-2'>
+          <div className='flex items-center'>
             <UilTemperature size={18}/>
             <p>Feels Like</p>
           </div>
-          <p className='text-2xl ml-6'>66°</p>
+          <p className='text-2xl'>&nbsp;66°</p>
         </div>
-        <div>
-          <div className='flex items-center space-x-1'>
+        <div className='bg-[#7eb8e0]/30 rounded-xl flex flex-col items-center justify-center py-2'>
+          <div className='flex items-center'>
+            <UilSun size={20}/>
+            <p>Sunrise</p>
+          </div>
+          <p className='text-xl'>06:54 AM</p>
+        </div>
+        <div className='bg-[#7eb8e0]/30 rounded-xl flex flex-col items-center justify-center py-2'>
+          <div className='flex items-center'>
+            <UilSunset size={20}/>
+            <p>Sunset</p>
+          </div>
+          <p className='text-xl'>07:13 PM</p>
+        </div>
+        <div className='bg-[#7eb8e0]/30 rounded-xl flex flex-col items-center justify-center py-2'>
+          <div className='flex items-center'>
             <UilTear size={18}/>
             <p>Humidity</p>
           </div>
-          <p className='text-2xl ml-6'>75%</p>
+          <p className='text-2xl'>75%</p>
         </div>
-        <div>
-          <div className='flex items-center space-x-1'>
+        <div className='bg-[#7eb8e0]/30 rounded-xl flex flex-col items-center justify-center py-2'>
+          <div className='flex items-center'>
             <UilWind size={18}/>
             <p>Wind</p>
           </div>
-          <p className='text-2xl ml-6'>12 mph</p>
+          <p className='text-2xl'>12 mph</p>
+        </div>
+        <div className='bg-[#7eb8e0]/30 rounded-xl flex flex-col items-center justify-center py-2'>
+          <div className='flex items-center'>
+            <UilWind size={18}/>
+            <p>UV Index</p>
+          </div>
+          <p className='text-2xl'>6</p>
         </div>
       </div>
     </div>
