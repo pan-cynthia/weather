7b92607e8d87f630a922ec7f4fbaea7bb90b5298
Cynthia Pan <cynthiapan11@gmail.com>
2023-09-19 19:30:21 -0700

    make search box edges rounded

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,7 +4,7 @@ import { UilSearch } from '@iconscout/react-unicons'
 function Search() {
   return (
     <div className='mx-auto flex justify-around my-6 w-5/6'>
-      <div className='flex w-5/6 space-x-2 bg-white rounded-sm'>
+      <div className='flex w-5/6 space-x-2 bg-white rounded-full'>
         <UilSearch className='text-slate-400 m-2' size={25}/>
         <input type="text" placeholder='Search' className='outline-none'/>
       </div>
