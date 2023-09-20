88ec2686408bc763930c0a1f8685bf886a3fb1fb
Cynthia Pan <cynthiapan11@gmail.com>
2023-09-20 14:35:18 -0700

    reposition forecast + conditions components

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,17 @@ import Conditions from './components/Conditions.jsx'
 
 function App() {
   return (
-    <div className='mx-auto max-w-screen-full bg-gradient-to-br from-[#2f99e5] to-[#3172af] h-fit'>
+    <div className='mx-auto max-w-screen-full bg-gradient-to-br from-[#2f99e5] to-[#3172af] h-screen'>
       <div className='flex justify-between items-center p-5'>
         <DateTime/>
         <Search/>
       </div>
-      <div className='px-32 pb-5'>
-        <Details/>
-        <Forecast title='Hourly Forecast' icon='UilClock'/>
-        <Forecast title='Daily Forecast' icon='UilCalender'/>
+      <Details/>
+      <div className='mx-auto mt-10 flex justify-between w-2/3 space-x-5'>
+        <div className='w-4/5 flex flex-col justify-between'>
+          <Forecast title='Hourly Forecast' icon='UilClock'/>
+          <Forecast title='Daily Forecast' icon='UilCalender'/>
+        </div>
         <Conditions/>
       </div>
     </div>
