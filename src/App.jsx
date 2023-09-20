212dfe5d88b424b9641b933279e4d81b1ac75778
Cynthia Pan <cynthiapan11@gmail.com>
2023-09-20 14:17:00 -0700

    resize screen

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import Search from './components/Search.jsx'
 import DateTime from './components/DateTime.jsx'
 import Details from './components/Details.jsx'
 import Forecast from './components/Forecast.jsx'
-import Conditions from './Conditions.jsx'
+import Conditions from './components/Conditions.jsx'
 
 function App() {
   return (
-    <div className='mx-auto m-5 max-w-screen-lg bg-gradient-to-br from-[#2f99e5] to-[#3172af] h-fit shadow-xl shadow-gray-400'>
+    <div className='mx-auto max-w-screen-full bg-gradient-to-br from-[#2f99e5] to-[#3172af] h-fit'>
       <div className='flex justify-between items-center p-5'>
         <DateTime/>
         <Search/>
