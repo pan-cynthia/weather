e686c394dbb4a0031184126e2b571b09fd0cc76b
Cynthia Pan <cynthiapan11@gmail.com>
2023-09-20 14:03:14 -0700

    move search bar to top right corner

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,12 @@ import Conditions from './Conditions.jsx'
 
 function App() {
   return (
-    <div className='mx-auto m-5 max-w-screen-lg rounded-lg bg-gradient-to-br from-[#2f99e5] to-[#3172af] h-fit shadow-xl shadow-gray-400'>
-      <DateTime/>
-      <div className='px-32 pb-5'>
+    <div className='mx-auto m-5 max-w-screen-lg bg-gradient-to-br from-[#2f99e5] to-[#3172af] h-fit shadow-xl shadow-gray-400'>
+      <div className='flex justify-between items-center p-5'>
+        <DateTime/>
         <Search/>
+      </div>
+      <div className='px-32 pb-5'>
         <Details/>
         <Forecast title='Hourly Forecast' icon='UilClock'/>
         <Forecast title='Daily Forecast' icon='UilCalender'/>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,12 +3,12 @@ import { UilSearch } from '@iconscout/react-unicons'
 
 function Search() {
   return (
-    <div className='mx-auto flex justify-around my-6 w-5/6'>
-      <div className='flex w-5/6 space-x-2 bg-white rounded-full'>
-        <UilSearch className='text-slate-400 m-2' size={25}/>
+    <div className='flex justify-end w-1/2'>
+      <div className='flex w-5/6 space-x-1 bg-white rounded-lg'>
+        <UilSearch className='text-slate-400 m-2' size={15}/>
         <input type="text" placeholder='Search' className='outline-none'/>
       </div>
-      <div className='flex justify-center items-center w-1/6 space-x-2 text-white'>
+      <div className='flex justify-center items-center w-1/6 space-x-2 text-white font-medium'>
         <button name='metric'>°C</button>
         <p>|</p>
         <button name='imperial'>°F</button>
