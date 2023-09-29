ec3a7f839c2182ee041cede13d9af6d1d04691da
Cynthia Pan <cynthiapan11@gmail.com>
2023-09-29 10:16:40 -0700

    populate forecast fields with fetched api data

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { UilClock, UilCalender } from '@iconscout/react-unicons'
 
-function Forecast({title, icon}) {
+function Forecast({title, icon, datetime, temperature, temperature2}) {
   return (
     <div className='bg-[#007cbc]/70 rounded-lg p-5 w-full'>
       <div className='flex items-center mb-2 space-x-1.5 text-[#7eb8e0]'>
@@ -9,31 +9,19 @@ function Forecast({title, icon}) {
         <p className=' font-medium uppercase'>{title}</p>
       </div>
       <div className='grid grid-cols-5 divide-x divide-[#7eb8e0]/25 text-white'>
-        <div className='flex flex-col items-center'>
-          <p>02:00 PM</p>
-          <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="sunny" className='w-12 my-1'/>
-          <p>60°</p>
-        </div>
-        <div className='flex flex-col items-center'>
-          <p>03:00 PM</p>
-          <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="sunny" className='w-12 my-1'/>
-          <p>60°</p>
-        </div>
-        <div className='flex flex-col items-center'>
-          <p>04:00 PM</p>
-          <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="sunny" className='w-12 my-1'/>
-          <p>60°</p>
-        </div>
-        <div className='flex flex-col items-center'>
-          <p>05:00 PM</p>
-          <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="sunny" className='w-12 my-1'/>
-          <p>60°</p>
-        </div>
-        <div className='flex flex-col items-center'>
-          <p>06:00 PM</p>
-          <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="sunny" className='w-12 my-1'/>
-          <p>60°</p>
-        </div>
+        {temperature.map((t, i) => (
+          <div key={i} className='flex flex-col items-center'>
+            <p>{datetime[i]}</p>
+            <img src="http://openweathermap.org/img/wn/01d@2x.png" alt="sunny" className='w-12 my-1'/>
+            {temperature2 ?
+              <div className='flex space-x-2'>
+                <p className='text-white/60 font-medium'>L:{temperature2[i].toFixed()}°</p> 
+                <p>H:{temperature[i].toFixed()}°</p>
+              </div> :
+              <p>{temperature[i].toFixed()}°</p>
+            }
+          </div>
+        ))}
       </div>
     </div>
   )
